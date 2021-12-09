33e4fcc59cb27615aaba868172659b1a6a5ae0ae
Gabrielle_Galarraga <94385818+Gabba19@users.noreply.github.com>
2021-12-09 10:16:34 -0500

    Create netsize.sh
    Creación de la carpeta Netsize.sh Parte 1 del Deber 1.10.3

---
+++ Tare 1/netsize.sh
@@ -0,0 +1,6 @@
+# EJERCICIO PARTE 1
+1. Escriba un guión que tome uno de estos archivos y determine el número de filas (polinizadores) y columnas (plantas). Tenga en cuenta que
+las columnas están separadas por espacios y que hay un espacio en
+al final de cada línea. Su guión debería regresar.
+
+La ejecución se realiza en el script <netsize.sh>
