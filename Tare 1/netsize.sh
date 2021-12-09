d6632963cf4b835ef1e38fb641249f7b4bf168fc
Gabrielle_Galarraga <94385818+Gabba19@users.noreply.github.com>
2021-12-09 12:08:22 -0500

    Update netsize.sh
    Creacion del Codigo para contar las filas del Archivo Saavedra2013/n1.txt

--- Tare 1/netsize.sh
+++ Tare 1/netsize.sh
@@ -4,5 +4,9 @@ las columnas están separadas por espacios y que hay un espacio en
 al final de cada línea. Su guión debería regresar.
 
 La ejecución se realiza en el script <netsize.sh>
+
 Codigo para contar las columnas del Archivo Saavedra2013/n1.txt
 head -n 1 FILE | awk '{print NF}'
+
+Codigo para contar las filas del Archivo Saavedra2013/n1.txt
+wc -l FILE
