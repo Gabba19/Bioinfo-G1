6c179dd8e5f856c5f16d99345f3cc0feb7258348
Gabrielle_Galarraga <94385818+Gabba19@users.noreply.github.com>
2021-12-09 12:17:53 -0500

    Update netsiza_all.sh
    llamado a la carpeta Saavedra2013

--- Tare 1/netsiza_all.sh
+++ Tare 1/netsiza_all.sh
@@ -4,3 +4,6 @@ las columnas están separadas por espacios y que hay un espacio en
 al final de cada línea. Su guión debería regresar.
 
 La ejecución se realiza en el script <netsize_all.sh> 
+El mismo paso de antes llamamos a la carpeta saveedra2013 
+$cd Saavedra2013 
+ 
